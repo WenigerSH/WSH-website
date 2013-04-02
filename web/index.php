ae304ea7abba5a78bd03ea29030544a5a1d94d04
Bartosz Rychlicki <bartosz.rychlicki@gmail.com>
2013-04-02 10:10:47 +0200

    clock overtweak

--- web/index.php
+++ web/index.php
@@ -11,7 +11,7 @@ $app['debug'] = false;
 $app->get('/', function() use ($app) {
 	$nowTimeStamp = strtotime("now");
 	$startTimeStamp = strtotime("29 march 2013 13:00:00");
-	$endTimeStamp = strtotime("2 april 2013 13:00:00");
+	$endTimeStamp = strtotime("2 april 2013 17:00:00");
 	return $app['twig']->render('homepage.html.twig', compact('nowTimeStamp', 'startTimeStamp', 'endTimeStamp'));
 });
 
